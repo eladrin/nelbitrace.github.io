7dd946e861446976e1929849e4ae3fd26a3ca253
nelbi <nelbitrace@gmail.com>
2017-02-09 00:39:12 +0900

    색상추가

--- mamul/main.js
+++ mamul/main.js
@@ -22,12 +22,13 @@ function renderList() {
 		} else if (mamul.e.getTime() < now.getTime()) {
 			//풀 젠
 			time = "풀젠 " + timeToString(now.getTime() - mamul.e.getTime()) + " 초과";
+			$row.addClass("full");
 		} else {
 			//초 젠과 풀젠 사이
 			time = "풀젠까지 " + timeToString(mamul.e.getTime() - now.getTime()) + " 남음";
 		}
 		
-		$row.find(".time").html(time + "<br>" + mamul.s + "<br>" + mamul.e + "<br>.");
+		$row.find(".time").html(time);
 		
 		$table.append($row);
 	}
