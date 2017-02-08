63dbbd5e646a816a9710ffb7664bd33a3a17cac7
nelbi <nelbitrace@gmail.com>
2017-02-08 22:11:52 +0900

    오타 수정

--- mamul/main.js
+++ mamul/main.js
@@ -9,7 +9,7 @@ function renderList() {
 	
 	for (var i = 0; i < mamulList.A.length; i++) {
 		var $row = $(".rowTemplate").clone().removeClass("rowTemplate");
-		$row.find(".name").setText(mamulList.A[i].name);
+		$row.find(".name").text(mamulList.A[i].name);
 		$table.append($row);
 	}
 	
